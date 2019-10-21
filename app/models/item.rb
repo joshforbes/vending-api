c2f5561ba3cb17c4bffc59787aef9da0afe6bb17
Josh Forbes <jdforbes82@gmail.com>
2019-10-21 08:22:14 -0400

    Remove duplicates from items query
    Because:
    - This vending machine is only going to list one of each item with the same name and cost
    
    This commit:
    - Adds a `one_of_each` scope that selects distinct on `name` and `cost`
    - Uses this scope on the `ItemsQuery`

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   validates :name, presence: true
   validates :cost, presence: true, numericality: {greater_than: 0, only_integer: true}
+
+  scope :one_of_each, -> { select('DISTINCT ON (name, cost) *').all }
 end
